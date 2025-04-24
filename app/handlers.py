3d6d1b98c03dfd6028daf88407fa36583203894b
Sofiya Zhuravleva <ssz_02@bk.ru>
2025-04-24 22:41:02 +0300

    добавила валуе

--- app/handlers.py
+++ app/handlers.py
@@ -23,20 +23,20 @@ class Registration(StatesGroup):
 async def cmd_start(message: Message, state: FSMContext):
     if (not db.user_exist(message.from_user.id)):
         await state.set_state(Registration.user_name)
-        await message.answer(RegistrationText.HELLO_MESSAGE)
+        await message.answer(RegistrationText.HELLO_MESSAGE.value)
 
 
 @router.message(Registration.user_name)
 async def add_name(message: Message, state: FSMContext):
     await state.update_data(name=message.text)
     await state.set_state(Registration.notification)
-    await message.answer(RegistrationText.RECEIVE_NOTIFICATIONS) #да нет клавиатура 
+    await message.answer(RegistrationText.RECEIVE_NOTIFICATIONS.value) #да нет клавиатура 
 
 @router.message(Registration.notification)
 async def add_name(message: Message, state: FSMContext):
     if (message.text).lower() in [YesNoAnswer.YES, YesNoAnswer.NO]:
         await state.update_data(notification = 1 if message.text.lower() == YesNoAnswer.YES else 2)
-        await message.answer(RegistrationText.SUCCESSFUL_REGISTRATION) #меню клавиатура
+        await message.answer(RegistrationText.SUCCESSFUL_REGISTRATION.value) #меню клавиатура
         data = await state.get_data()
         db.add_user(message.from_user.id, data['user_name'], data['notification'])
         await state.clear()
